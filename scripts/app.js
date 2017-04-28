e9c438b1fb7fa97f8130d68387c320d56d8168a0
Infologs <webboostings@gmail.com>
2017-04-28 16:43:58 +0530

    Angular JS Form validation

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("app01", []);
+
+app.controller("controller1",function(){
+	this.FirstName = "";
+	this.EmailID = "";
+	this.PhoneNo = "";
+
+	this.SubmitForm = function(){
+		alert(this.FirstName + " " + this.EmailID + " " + this.PhoneNo);
+	}
+});
